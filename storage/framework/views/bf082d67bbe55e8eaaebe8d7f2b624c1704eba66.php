e4b35184e5f67335bd16c3274b77168ed9cb1bef
rinisusilawati <65141682+rinisusila11@users.noreply.github.com>
2022-12-19 17:51:44 +0700

    Add files via upload

---
+++ storage/framework/views/bf082d67bbe55e8eaaebe8d7f2b624c1704eba66.php
@@ -0,0 +1,39 @@
+<?php $__env->startSection('content'); ?>
+<!-- <section class="content-header">
+
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+         <li><a href="dashboard"><i class="fa fa-dashboard"></i>Add Notes</a></li>
+    </ol>
+    </section> -->
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="box">
+        <div class="box-header with-border">
+          <h3 class="box-title">All Notes</h3>
+
+          <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
+                    title="Collapse">
+              <i class="fa fa-minus"></i></button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+              <i class="fa fa-times"></i></button>
+          </div>
+        </div>
+        <div class="box-body">
+          HARUS BISA DIISI SEMUA NOTES BARU
+        </div>
+        <!-- /.box-body -->
+        
+      
+
+        <!-- /.box-footer-->
+      </div>
+      <!-- /.box -->
+
+    </section>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\ASUS\Videos\note-taking-weblaravel--master\resources\views/dashboard.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/c08c43c202e92c5e54327754a00fcbf932791985.php
@@ -0,0 +1,7 @@
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>2022</b> 
+    </div>
+    <strong> Pemprograman Integratif</a></strong> 
+   
+  </footer><?php /**PATH C:\Users\ASUS\Videos\note-taking-weblaravel--master\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
